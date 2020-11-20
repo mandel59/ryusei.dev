d67200cb744c0b24c92a65ad9b7faa5b352eba7e
Ryusei Yamaguchi <mandel59@gmail.com>
2020-11-20 16:12:41 +0900

    clean up and redirect to mandel59.github.io

--- components/jsonld.tsx
+++
@@ -1,8 +0,0 @@
-function JsonLd(props: { data: object }) {
-    return <script type="application/ld+json" dangerouslySetInnerHTML={{
-        // Escape slashes to prevent xss.
-        // See: https://sophiebits.com/2012/08/03/preventing-xss-json.html
-        __html: JSON.stringify(props.data).replace(/\//g, "\\/")
-    }} />
-}
-export default JsonLd
